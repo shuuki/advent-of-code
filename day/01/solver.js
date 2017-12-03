1d17ce3f24c2188315c7b96066b29494f50e6182
shuuki <shuuki@users.noreply.github.com>
2017-12-02 22:05:08 -0700

    first day first idea solution, unverified

---
+++ day/01/solver.js
@@ -0,0 +1,32 @@
+function uncaptcha (input) {
+
+	let sum = 0;
+	let seq = input.split('');
+	let check = 0;
+
+	for (let i = 0; i < seq.length; i++) {
+
+		let add = 0;
+
+		if (i < input.length-1) {
+			add = compare(input[i], input[i+1])
+		}
+		else if (i === input.length-1) {
+			add = compare(input[input.length-1], input[0])
+		}
+
+		sum = sum + Number(add)
+		check += add
+
+	}
+	return {sum, check};
+}
+
+function compare (f,s) {
+
+	let add = 0;
+
+	if (f === s) { add = s; } else { add = 0; }
+
+	return add;
+}
